ffbdcd251f1d48be947a02707ec0e8a8a436bbba
Only566 <201803020134@stu.hnie.edu.cn>
2021-12-24 17:15:58 +0800

    feat(backend): unit test search user

--- packages/backend/tests/services/users.spec.ts
+++ packages/backend/tests/services/users.spec.ts
@@ -5,18 +5,29 @@ import { expect } from 'chai'
 before(async () => {
   await DAOMain()
 })
-after(async () => {
-  await UsersService.Model.deleteMany({})
+after(() => {
   process.exit(0)
 })
 
 describe('Users Service', function () {
-  it('should test add user.', async function () {
+  afterEach(async () => {
+    await UsersService.Model.deleteMany({})
+  })
+  it('should add user.', async function () {
     expect(await UsersService.exist('test')).to.be.eq(false)
-    await UsersService.addUser({
-      username: 'test',
-      passwordHash: 'test'
-    })
+    const nu = await UsersService.add({ username: 'test', passwordHash: 'test' })
+    expect(nu).not.to.be.undefined
+    expect(nu).property('passwordHash')
+      .to.be.eq('test')
     expect(await UsersService.exist('test')).to.be.eq(true)
+  })
+  it('should search user by username.', async function () {
+    await UsersService.add({ username: 'test', passwordHash: 'test' })
+    const [ u ] = await UsersService.search('t').limit(1)
+    expect(u).not.to.be.undefined
+    expect(u.username).to.be.eq('test')
+    expect(
+      await UsersService.search('m').count()
+    ).to.be.eq(0)
   })
 })
