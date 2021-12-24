e0618afd52de7d0b6ef4e2204e64c2434da560e7
Only566 <201803020134@stu.hnie.edu.cn>
2021-12-24 17:14:54 +0800

    upd(backend): new id property of user

--- packages/backend/src/dao/user.ts
+++ packages/backend/src/dao/user.ts
@@ -1,12 +1,20 @@
 import { Schema, model } from 'mongoose'
 
 export interface User {
+  /** 用户唯一 id */
+  id: number
+  /** 用户名 */
   username: string
   /** 加密的用户密码 */
   passwordHash: string
 }
 
 const userSchema = new Schema<User>({
+  id: {
+    type: Number,
+    unique: true,
+    required: true
+  },
   username: {
     type: String,
     required: true
--- packages/backend/src/services/users.ts
+++ packages/backend/src/services/users.ts
@@ -1,13 +1,21 @@
 import { UserModel, User } from '../dao/user'
+import { Seq } from '../utils'
 
 export namespace UsersService {
   export const Model = UserModel
-  export async function addUser(u: User) {
-    if(await exist(u.username)) {
+  export type U = Omit<User, 'id'>
+  export function search(key: string) {
+    return Model.find({ username: new RegExp(`${key}.*`) })
+  }
+  export async function add(u: U) {
+    if(await exist(u.username))
       throw new Error('the user has existed')
-    }
-    const newU = new UserModel(u)
-    await newU.save()
+    return new UserModel(Object.assign({
+      id: await Seq.auto('users')
+    }, u)).save()
+  }
+  export async function del(id: number) {
+    Model.findOneAndDelete({ id })
   }
   export async function exist(uname: string) {
     return await UserModel.findOne({ username: uname }) !== null
