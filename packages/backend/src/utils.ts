ce137a6acd5581db9f3b2a063e6021f608c8eeeb
Only566 <201803020134@stu.hnie.edu.cn>
2021-12-24 15:30:28 +0800

    feat(backend): security util

--- packages/backend/src/utils.ts
+++ packages/backend/src/utils.ts
@@ -1,10 +1,18 @@
 import { Server } from 'ws'
+import { createHash } from 'crypto'
 
-export namespace Utils {
+namespace Utils {
   export namespace WS {
     export let s: Server | null = null
     export function register(ns: Server) {
       s = ns
     }
   }
+  export namespace Security {
+    const md5 = createHash('md5')
+    export function encrypt(plaintext: string) {
+      return md5.update(plaintext).digest('hex')
+    }
+  }
 }
+export = Utils
