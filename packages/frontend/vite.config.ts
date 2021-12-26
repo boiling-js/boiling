a6a0e83fdf58be455bf4d69b2aa2d20f83740cc0
Only566 <201803020134@stu.hnie.edu.cn>
2021-12-26 16:46:26 +0800

    chore(frontend): update vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,7 +4,17 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   server: {
-    port: 17980
+    port: 17980,
+    proxy: {
+      // 选项写法
+      '/api': {
+        // http://localhost:17980/api/users/123
+        // http://localhost:6532/users/123
+        target: 'http://localhost:6532',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   plugins: [vue(), ElementPlus()]
 })
