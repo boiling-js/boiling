94eca028916ecac62be8081f9acab9de5ae11fc2
yijie4188 <yijie4188@gmail.com>
2021-12-26 21:58:23 +0800

    fix(core): remove security unit test

--- packages/core/tests/utils.spec.ts
+++ packages/core/tests/utils.spec.ts
@@ -3,16 +3,4 @@ import { expect } from 'chai'
 import { Utils } from '../src/utils'
 
 describe('Utils', function () {
-  describe('Security', function () {
-    const { Security } = Utils
-
-    it('should password is match passwordHash.', function () {
-      expect(
-        Security.match('123', Security.encrypt('123'))
-      ).to.be.eq(true)
-      expect(
-        Security.match('456', Security.encrypt('123'))
-      ).to.be.eq(false)
-    })
-  })
 })
