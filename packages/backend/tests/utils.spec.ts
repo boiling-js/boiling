9a06f5643d192b74add0560f2ebd1ff9490071cb
yijie4188 <yijie4188@gmail.com>
2021-12-26 14:58:22 +0800

    feat(backend): unit test Security match

--- packages/backend/tests/utils.spec.ts
+++ packages/backend/tests/utils.spec.ts
@@ -1,9 +1,16 @@
+import { expect } from 'chai'
+
 import { Security } from '../src/utils'
 
 describe('Utils', function () {
   describe('Security', function () {
     it('should password is match passwordHash.', function () {
-      Security.match('123', Security.encrypt('123'))
+      expect(
+        Security.match('123', Security.encrypt('123'))
+      ).to.be.eq(true)
+      expect(
+        Security.match('456', Security.encrypt('123'))
+      ).to.be.eq(false)
     })
   })
 })
