eb7bc85bac86f05856d0f2cf25f72e51c5171a1f
yijie4188 <yijie4188@gmail.com>
2022-01-08 17:37:24 +0800

    refactor: refactor `statusIn` interface and refactor `Login.vue` page

--- packages/frontend/src/views/Login.vue
+++ packages/frontend/src/views/Login.vue
@@ -3,27 +3,27 @@
     <div class="login-box">
       <h3 class="title">欢迎回来~</h3>
       <div class="login-input">
-        <div class="label"> 账号： </div>
-        <el-input v-model="account.id" placeholder="请输入账号" />
-        <div class="label"> 密码： </div>
-        <el-input v-model="account.password" placeholder="请输入密码" show-password />
+        <div class="label">账号：</div>
+        <el-input v-model="account.id" placeholder="请输入账号"/>
+        <div class="label">密码：</div>
+        <el-input v-model="account.password" placeholder="请输入密码" show-password/>
         <div class="bottom-word">记住密码</div>
       </div>
-      <el-button
-        class="btn"
-        type="primary"
-        @click="login">
-        登录
-      </el-button>
-      <div class="register">需要新的账号？ <span class="click" @click="register">注册</span></div>
+      <el-button type="primary" @click="login().then(
+        () => $router.push('/home'),
+        () => $message.error('登录失败')
+      )" v-text="'登录'"/>
+      <div class="register">
+        需要新的账号？
+        <span class="ln" @click="$router.push('/register')">注册</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, onUnmounted, reactive } from 'vue'
-import { useRouter } from 'vue-router'
-import { ElInput, ElButton,  ElMessage } from 'element-plus'
+import { ElInput, ElButton } from 'element-plus'
 import { api } from '../api'
 import { Users } from '@boiling/core'
 import { useStore } from 'vuex'
@@ -36,25 +36,12 @@ const
   store = useStore(),
   account = reactive<Account>({
     id: '',
-    password: '',
     status: 'online',
-    avatar: ''
+    password: ''
   }),
-  router = useRouter(),
   login = async () => {
-    try {
-      const { id, ...status } = account
-      const res = await api.user(id).status.add(status)
-      store.commit('setUser', res)
-      ElMessage.success('登录成功！')
-      await router.push('/home')
-    } catch (e) {
-      // @ts-ignore
-      ElMessage.error(e.response.data)
-    }
-  },
-  register = () => {
-    router.push('/register')
+    const { id, ...status } = account
+    store.commit('setUser', await api.user(+id).status.add(status))
   }
 
 onMounted(() => {
@@ -71,9 +58,9 @@ onUnmounted(() => {
   $h: 320px;
   $p: 35px;
 
-  background-size: cover;
-  background-position: center;
   background-image: url("../assets/img/bg/1.png");
+  background-position: center;
+  background-size: cover;
   > .login-box {
     position: fixed;
     top: calc(50vh - #{$h} / 2 - #{$p});
@@ -101,13 +88,13 @@ onUnmounted(() => {
         }
       }
     }
-    > .btn {
+    > .el-button {
       margin: 10px 0;
       width: $w;
     }
     > .register {
       font-size: 10px;
-      > .click {
+      > .ln {
         cursor: pointer;
         &:hover {
           color: var(--color-theme);
