e33482cbbced5b5dbca8040f6759e27069657101
yijie4188 <yijie4188@gmail.com>
2022-01-11 23:47:55 +0800

    refactor(frontend): refactor user component

--- packages/frontend/src/components/User.vue
+++ packages/frontend/src/components/User.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="user-info-card">
-    <div class="avatar">
-      <img :src="`/api/${info.avatar}`" alt="">
+  <div class="user">
+    <div class="bg">
+      <div class="operates">
+        <span v-if="!isFriend"
+              class="material-icons"
+              @click="addUserDialog = true">add</span>
+        <span v-if="isFriend"
+              class="material-icons"
+              @click="$emit('chat')">chat_bubble_outline</span>
+        <span v-if="isFriend"
+              class="material-icons"
+              @click="addUserDialog = true">settings</span>
+      </div>
     </div>
+    <div class="avatar" :style="{
+      backgroundImage: `url(/api${info.avatar})`
+    }"/>
     <div class="info">
-      ID：{{ info.id }}
-      <br>
-      昵称：{{ info.remark || info.username }}
-    </div>
-    <div class="operate">
-      <span
-        v-if="!isFriend"
-        class="icon material-icons"
-        @click="addUserDialog = true">add</span>
-      <span
-        v-if="isFriend"
-        class="icon material-icons"
-        @click="$emit('chat')">chat_bubble_outline</span>
-      <span
-        v-if="isFriend"
-        class="icon material-icons"
-        @click="addUserDialog = true">settings</span>
+      {{ info.remark || info.username }}<span class="id">#{{ info.id }}</span>
     </div>
     <el-dialog
       v-model="addUserDialog"
@@ -32,12 +29,8 @@
         </el-form-item>
         <el-form-item label="标签：">
           <el-select v-model="addUserForm.tags" multiple placeholder="请选择标签">
-            <el-option
-              v-for="item in addUserForm.tags"
-              :key="item"
-              :label="item"
-              :value="item">
-            </el-option>
+            <el-option v-for="item in addUserForm.tags" :key="item"
+                       :label="item" :value="item"/>
           </el-select>
           <el-input
             v-if="tagInputVisible"
@@ -78,10 +71,10 @@ import { useStore } from 'vuex'
 
 defineEmits(['chat'])
 const
+  store = useStore(),
   props = defineProps<{
     info: Users.Out | Users.FriendOut
   }>(),
-  store = useStore(),
   addUserDialog = ref(false),
   addUserForm = reactive<Omit<Users.Friend, 'id'>>({
     tags: [],
@@ -123,31 +116,65 @@ const
 </script>
 
 <style lang="scss" scoped>
-.user-info-card {
+div.user {
+  --bg-color: var(--color-auxi-regular);
+
+  position: relative;
   display: flex;
-  padding: 10px;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 30px 10px 10px;
   width: calc(100% - 20px);
-  height: 50px;
-  background-color: var(--color-auxi-regular);
+  height: 100px;
+  overflow: hidden;
+  color: var(--color-text-regular);
+  background-color: var(--bg-color);
   border-radius: 6px;
-  > div.avatar {
-    margin-right: 10px;
-    width: 50px;
-    height: 50px;
-    overflow: hidden;
-    border-radius: 50%;
-    > img {
-      width: 100%;
-      height: 100%;
+  > div.bg {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 60px;
+    background-color: #fff;
+    border-radius: 6px 6px 0 0;
+    > div.operates {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      > span.material-icons {
+        padding: 4px;
+        margin: 0 5px;
+        cursor: pointer;
+        background-color: var(--bg-color);
+        border-radius: 6px;
+        opacity: 0.5;
+        transition: 0.3s;
+        &:hover {
+          opacity: 1;
+        }
+      }
     }
   }
-  > div.info {
-    flex-grow: 1;
+  > div.avatar {
+    --size: 64px;
+
+    z-index: 10;
+    margin-right: 10px;
+    width: var(--size);
+    height: var(--size);
+    overflow: hidden;
+    background-size: cover;
+    border: 6px solid var(--bg-color);
+    border-radius: 50%;
   }
-  > div.operate {
-    > span.icon {
-      margin: 0 5px;
-      cursor: pointer;
+  > div.info {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    font-weight: bold;
+    > span.id {
+      color: var(--color-text-secondary);
     }
   }
 }
