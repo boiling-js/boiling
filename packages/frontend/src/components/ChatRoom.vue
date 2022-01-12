71cf574b9fb6edc6ea5b3a79af977487a562624f
Only566 <201803020134@stu.hnie.edu.cn>
2022-01-12 22:30:19 +0800

    feat(frontend): and chat edit message

--- packages/frontend/src/components/ChatRoom.vue
+++ packages/frontend/src/components/ChatRoom.vue
@@ -7,24 +7,37 @@
       <span>{{ props.friend.remark || props.friend.username }}</span>
     </div>
     <div class="room">
+      <div class="content">
 
+      </div>
+      <div class="message-input">
+        <el-input
+          v-model="editingMessage"
+          type="textarea"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Users } from '@boiling/core'
+import { ElInput } from 'element-plus'
+import { ref } from 'vue'
 
 defineEmits(['back'])
 const
   props = defineProps<{
     friend: Users.FriendOut
-  }>()
+  }>(),
+  editingMessage = ref<string>('')
 </script>
 
 <style scoped lang="scss">
 .chat {
+  display: flex;
+  flex-direction: column;
   width: 100%;
+  height: 100%;
   > div.top-bar {
     display: flex;
     align-items: center;
@@ -38,8 +51,17 @@ const
     }
   }
   > div.room {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
     padding: 20px;
     width: calc(100% - 40px);
+    > div.content {
+      flex-grow: 1;
+    }
+    > div.message-input {
+      height: 60px;
+    }
   }
 }
 </style>
