140749b8414082d3861b00dd9c7e76227cb5253b
yijie4188 <yijie4188@gmail.com>
2022-01-14 16:51:18 +0800

    refactor(backend): refactor use pagination hook

--- packages/backend/src/hooks/usePagination.ts
+++ packages/backend/src/hooks/usePagination.ts
@@ -8,12 +8,16 @@ interface SearchService<M extends mongoose.Model<any>> {
 
 export default function usePagination<
   M extends mongoose.Model<any>, S extends SearchService<M>
->(searchService: S, searchQuery: SearchQuery) {
+>(searchService: S, searchQuery: {
+  key: string
+  num?: number
+  page?: number
+}) {
   const { key, page = 0, num = 10 } = searchQuery
   const s = searchService.search(key)
   return async <T>(mapFun?: Parameters<Array<InstanceType<S['Model']>>['map']>[0]) => {
     const [count, items] = await Promise.all([
-      s.clone().count(), s.limit(+num).skip((+page)*(+num)).then(items => mapFun ? items.map(mapFun) : items)
+      s.clone().count(), s.limit(num).skip(page*num).then(items => mapFun ? items.map(mapFun) : items)
     ])
     return <Pagination<T>>{ count, items }
   }
