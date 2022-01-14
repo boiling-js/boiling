bd3df0ea2cfe965e9bdb2ea8e75a15c360c961de
yijie4188 <yijie4188@gmail.com>
2022-01-14 20:41:53 +0800

    fix(core): typo

--- packages/core/tests/schemastery-interface.spec.ts
+++ packages/core/tests/schemastery-interface.spec.ts
@@ -1,6 +1,6 @@
 import Schema from 'schemastery'
 import { expect } from 'chai'
-import '../src/schemastry-interface'
+import '../src/schemastery-interface'
 
 it('should use schemastry interface.', () => {
   const UserOut = Schema.interface({
@@ -11,13 +11,16 @@ it('should use schemastry interface.', () => {
     })
   })
   expect(UserOut({
-    id: 1, name: 'John'
+    id: 1, name: 'John', tags: [],
+    friend: { name: 'John' }
   })).to.not.have.property('id')
   expect(UserOut({
-    id: 1, tags: ['John']
+    id: 1, name: 'John', tags: ['John'],
+    friend: { name: 'John' }
   })).property('tags').to.be.contain('John')
   expect(UserOut({
-    id: 1, friend: {
+    id: 1, name: 'John', tags: ['John'],
+    friend: {
       id: 1,
       name: 'John'
     }
