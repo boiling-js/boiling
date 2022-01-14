a73309b2578074555f332b4649182e7eaff95ebd
yijie4188 <yijie4188@gmail.com>
2022-01-14 16:52:09 +0800

    refactor(backend): use target hook

--- packages/backend/src/hooks/useTarget.ts
+++ packages/backend/src/hooks/useTarget.ts
@@ -1,11 +1,6 @@
 import useCurUser from './useCurUser'
 import { Session } from 'koa-session'
 
-export default function useTarget(params: Record<string, string>, session: Session | null, key = 'id') {
-  let myId: number
-  const id = params[key]
-  id === '@me'
-    ? (myId = useCurUser(session).id)
-    : (myId = +id)
-  return myId
+export default function useTarget(session: Session | null, id: number | '@me') {
+  return id === '@me' ? useCurUser(session).id : id
 }
