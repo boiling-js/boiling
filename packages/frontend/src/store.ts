5a30acc59440a513832b2340f4d45b647ff63126
yijie4188 <yijie4188@gmail.com>
2022-01-21 04:10:18 +0800

    refactor(frontend): refactor chat room

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -5,9 +5,9 @@ import { api } from './api'
 
 export default createStore({
   state: {
-    chatRoom: <ChatRoom>{
-    },
     isHiddenLeftSelector: false,
+    chatRoom: <ChatRoom.Model>{
+    },
     user: <Users.Out>{
       id: 0,
       username: '',
@@ -26,9 +26,6 @@ export default createStore({
     setUser(state, user: Users.Out) {
       state.user = user
     },
-    chatRoomPushUser(state, user: Users.Out) {
-      state.chatRoom.members.push(user)
-    },
     addFriend(state, friend: Users.Friend) {
       state.user.friends.push(friend)
     },
@@ -43,15 +40,15 @@ export default createStore({
   },
   actions: {
     async addFriend(context, friend: Users.Friend) {
-      await api.user(this.state.user.id).friends.add(friend)
+      await api.user('@me').friends.add(friend)
       context.commit('addFriend', friend)
     },
     async delFriend(context, id: number) {
+      await api.user('@me').friend(id).del()
       context.commit('delFriend', id)
-     await api.user(this.state.user.id).friend(id).del()
     },
     async updFriend(context, friend: Users.Friend) {
-      await api.user(this.state.user.id).friend(friend.id).upd({
+      await api.user('@me').friend(friend.id).upd({
         tags: friend.tags,
         remark: friend.remark
       })
