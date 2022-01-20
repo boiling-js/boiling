dc560ca752c3af5ae7540a44edf06fbca430c6f0
yijie4188 <yijie4188@gmail.com>
2022-01-21 02:43:03 +0800

    feat(backend): ws base logic

--- packages/backend/src/routes/ws.ts
+++ packages/backend/src/routes/ws.ts
@@ -1,76 +1,85 @@
+import Websocket from 'ws'
 import { Middleware } from 'koa-websocket'
-import { Messages } from '@boiling/core/src/message'
-import { WebSocket } from 'ws'
+import { Message } from '@boiling/core'
 
-const users = new Map<string, WebSocket>()
+class Sender {
+  constructor(public ws: Websocket) {
+    this.ws = ws
+  }
+  do(m: Message.Server) {
+    this.ws.send(JSON.stringify(m))
+  }
+  hello() {
+    this.do({
+      op: Message.Opcodes.HELLO,
+      d: { heartbeatInterval: 600000 }
+    })
+  }
+  ping() {
+    this.do({ op: Message.Opcodes.HEARTBEAT_ACK })
+  }
+  dispatch<M extends Message.PickTarget<Message.Opcodes.DISPATCH>>(t: M['t'], d: M['d']) {
+    this.do({
+      op: Message.Opcodes.DISPATCH,
+      // @ts-ignore
+      t, d
+    })
+  }
+}
 
+/**
+ * 基础流程：
+ * 服务端发送 hello 数据包给客户端，其中包含了心跳包的间隔时间
+ * 客户端接收到 hello 数据包后，发送鉴权数据包给服务端
+ * 鉴权通过后，服务端再下发其他的数据包给客户端
+ * 客户端按照 hello 包中的心跳包间隔时间，发送心跳包给服务端
+ * 服务端收到心跳包后，发送心跳包响应给客户端
+ *
+ * 异常处理：
+ * 1. 鉴权不通过，服务端会主动断开连接
+ * 2. 多次未发送心跳包，服务端会主动断开连接
+ * 3. 鉴权不通过不会下发其他数据包
+ */
 export const router: Middleware = async (context, next) => {
-  if (!/^\/ws.*/.test(context.url))
+  if (!context.url.startsWith('/ws'))
     return await next()
-
-  const { websocket: ws, request: req } = context
-
-  const { authorization } = Object.assign({
-    authorization:
-      req.headers['proxy-authorization'] ||
-      req.headers.authorization ||
-      req.query.authorization ||
-      ''
-  }, req.headers)
-  if (!authorization) {
-    ws.close(4003, 'Unauthorized')
-    return
-  }
-
-  let selfUid = ''
-  // 从请求头中获取用户名与密码
-  const [type, token] = authorization?.split(' ') as ['Basic', string]
-  switch (type) {
-    case 'Basic':
-      const [uname, pwd] = Buffer.from(token, 'base64').toString().split(':')
-      selfUid = `${ uname }@${ req.socket.remoteAddress }`
-      break
-    default:
-      ws.close(4003, 'Unauthorized')
-      return
-  }
-
-  users.set(selfUid, ws)
-  // 连接建立时发送当前在线用户列表
-  ws.send(JSON.stringify({
-    t: 'HELLO',
-    p: Array.of(...users.keys())
-  }))
-
-  ws.on('message', m => {
-    const message = JSON.parse(m.toString()) as Messages.Client
-    switch (message.t) {
-      case 'MESSAGE':
-
-        break
-      // 客户端触发 发送文件 事件，请求他人同意接收文件
-      case 'SEND_FILE':
-        const { uid: target, filename } = message.p
-        users.get(target)?.send(JSON.stringify({
-          t: 'RECEIVE_FILE',
-          p: { uid: selfUid, filename }
-        }))
-        break
-      // 客户端触发 接收文件 事件，允许他人向当前用户发送文件
-      case 'RECEIVE_FILE':
-        users.get(message.p)?.send(JSON.stringify({
-          t: 'SEND_FILE',
-          p: selfUid
-        }))
-        break
+  const { websocket: ws } = context
+  const sender = new Sender(ws)
+  sender.hello()
+  new Promise(((resolve, reject) => {
+    let isIdentified = false
+    // 五秒内发送鉴权
+    setTimeout(() => {
+      if (!isIdentified)
+        reject(new HttpError('REQUEST_TIMEOUT', '超时未发送鉴权'))
+    }, 5000)
+    ws.on('message', data => {
+      try {
+        const m = <Message.Client>JSON.parse(data.toString())
+        switch (m.op) {
+          case Message.Opcodes.HEARTBEAT:
+            sender.ping()
+            break
+          case Message.Opcodes.IDENTIFY:
+            const { d } = m
+            if (d.token === 'Basic hhhhhhh')
+              isIdentified = true
+            else {
+              reject(new HttpError('UNAUTHORIZED', '鉴权失败'))
+              return
+            }
+            break
+        }
+      } catch (e) {
+        reject(new HttpError('BAD_REQUEST', '错误的请求'))
+      }
+    })
+  })).catch(e => {
+    if (e instanceof HttpError)
+      ws.close(e.code + 4000, e.msg)
+    else {
+      ws.close(4500, '未知错误')
+      console.error(e)
     }
-  })
-  ws.on('close', () => {
-    console.log(`[I] ${ selfUid } is disconnected.`)
-    users.delete(selfUid)
-  })
-
-  ws.on('message', m => {
-    console.log(m.toString())
   })
 }
---
+++ packages/backend/tests/ws.spec.ts
@@ -0,0 +1,68 @@
+import Websocket from 'ws'
+import Koa from 'koa'
+import websockify from 'koa-websocket'
+import { expect } from 'chai'
+import { Message } from '@boiling/core'
+import DAOMain from '../src/dao'
+import { router as WSRouter } from '../src/routes/ws'
+import { UserModel } from '../src/dao/user'
+
+after(() => process.exit(0))
+
+describe('WS', function () {
+  const users = <Record<string, InstanceType<typeof UserModel>>>{
+    default: new UserModel({
+      id: 1001,
+      username: 'default',
+      avatar: 'default',
+      passwordHash: 'default'
+    })
+  }
+  const {
+    PORT = '19849',
+    HOST = '127.0.0.1'
+  } = {}
+  const app = websockify(new Koa())
+  app.keys = ['any']
+  app.ws.use(WSRouter)
+
+  after(async () => {
+    await UserModel.deleteMany()
+  })
+  before(() => new Promise<void>((resolve, reject) => {
+    app.listen(+PORT, HOST, () =>
+      DAOMain().then(resolve).catch(reject)
+    )
+  }).then(async () => {
+    await users.default.save()
+  }))
+
+  it('should connect ws server.', function () {
+    return new Promise<void>((resolve, reject) => {
+      const ws = new Websocket(`ws://${ HOST }:${ PORT }/ws`)
+      ws.on('open', () => {
+        ws.send(JSON.stringify({
+          op: Message.Opcodes.IDENTIFY,
+          d: {
+            token: 'Basic hhhhhhh'
+          }
+        }))
+      })
+      ws.on('message', m => {
+        console.log(m)
+        resolve()
+      })
+      ws.on('close', code => {
+        reject(new Error(`ws close: ${ code }`))
+      })
+    })
+  })
+  it('should connect ws server and throw `REQUEST_TIMEOUT` error.', function (done) {
+    this.timeout(5500)
+    const ws = new Websocket(`ws://${ HOST }:${ PORT }/ws`)
+    ws.on('close', code => {
+      expect(code).to.be.eq(4408)
+      done()
+    })
+  })
+})
