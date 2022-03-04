2048bc90ae985afcf79f5b2f0e0574e3e86fdd1c
Only566 <201803020134@stu.hnie.edu.cn>
2022-03-04 11:51:13 +0800

    feat(backend): todo push message

--- packages/backend/src/routes/chat-rooms.ts
+++ packages/backend/src/routes/chat-rooms.ts
@@ -1,6 +1,15 @@
-import Schema from 'schemastery'
+// import Schema from 'schemastery'
 import { Router } from '@boiling/core'
+// import { ChatRoomsService } from '../services/chat-rooms'
 
 export const router = new Router({
   prefix: '/chat-rooms' as '/chat-rooms'
 })
+  .post('/:chatRoomId/messages ', async ctx => {
+    // const [ oStr, createAt, senderId, ...receiverIds ] = ctx.params.chatRoomId.match(/^([\d+])(:\d:)+$/)
+    // const { content } = ctx.request.body
+    // await ChatRoomsService.pushMessage(+ctx.params.senderId, {
+    //   content: content,
+    //   createdAt: ctx.params.date
+    // }, true)
+  })
