b9f75b1e690172d897d2c57459da955706397ee7
Only566 <201803020134@stu.hnie.edu.cn>
2022-03-10 18:20:43 +0800

    fix(core): resolve path with prefix

--- packages/core/src/router.ts
+++ packages/core/src/router.ts
@@ -96,7 +96,7 @@ export class Router<O extends Router.Options, Docs> {
         if (item.pathRegexp.test(ctx.path)) {
           // @ts-ignore
           item.inn(ctx.request.body)
-          const { param, query } = Router.resolveSource(ctx.url, {
+          const { param, query } = Router.resolveSource(ctx.url.replace(new RegExp(`^${this.opts.prefix}`), ''), {
             query: item.queryTypes,
             param: item.paramTypes
           })
