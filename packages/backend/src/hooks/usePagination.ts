2d21133bc4d464da7aca2ccff1ec1537581beaa5
ijiefeng <ijiefeng@tencent.com>
2022-03-27 16:36:46 +0800

    feat(backend): support sort

--- packages/backend/src/hooks/usePagination.ts
+++ packages/backend/src/hooks/usePagination.ts
@@ -8,12 +8,12 @@ export interface SearchService<M extends mongoose.Model<any>> {
 
 export default function usePagination<
   M extends mongoose.Model<any>, S extends SearchService<M>
->(searchService: S, paginate: Paginate) {
+>(searchService: S, paginate: Paginate, order: [string, -1 | 0 | 1][] = []) {
   const { page = 0, num = 10 } = paginate
   return <T>(...args: Parameters<S['search']>) => {
     const s = searchService.search(...args)
     return Promise.all([
-      s.clone().count(), s.limit(num).skip(page*num)
+      s.clone().count(), s.limit(num).sort(order).skip(page*num)
     ]).then(([count, items]) => <Pagination<T>>{ count, items })
   }
 }
--- packages/backend/tests/hooks/usePagination.spec.ts
+++ packages/backend/tests/hooks/usePagination.spec.ts
@@ -3,9 +3,13 @@ import { expect } from 'chai'
 
 import usePagination from '../../src/hooks/usePagination'
 
-const testSchema = new Schema<{ name: string }>({
+const testSchema = new Schema<{ name: string, index: number }>({
   name: {
     type: String,
+    required: true
+  },
+  index: {
+    type: Number,
     required: true
   }
 })
@@ -25,7 +29,7 @@ describe('use Pagination', () => {
   before(async () => {
     await require('../../src/dao/index.ts').default()
     for (let i = 0; i < 48; i++) {
-      await TestModel.create({ name: `test${i}` })
+      await TestModel.create({ name: `test${i}`, index: i })
     }
   })
   after(async () => {
@@ -42,5 +46,15 @@ describe('use Pagination', () => {
     p = await usePagination(TestService, { page: 1, num: 15 })<{ name: string }>('')
     expect(p.items.length).to.be.eq(15)
     expect(p.items[0].name).to.be.eq('test15')
+  })
+  it('should get target test data by order.', async () => {
+    let p = await usePagination(
+      TestService, {}
+    )<{ name: string }>('')
+    expect(p.items[0].name).to.be.eq('test0')
+    p = await usePagination(
+      TestService, {}, [[ 'index', -1 ]]
+    )<{ name: string }>('')
+    expect(p.items[0].name).to.be.eq('test47')
   })
 })
