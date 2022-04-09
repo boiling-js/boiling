c9a633765fa6c9dcafa554d687356515069c30ee
yijie4188 <yijie4188@gmail.com>
2022-04-09 16:10:51 +0800

    fix(backend): remove useless code

--- packages/backend/src/routes/channels.ts
+++ packages/backend/src/routes/channels.ts
@@ -1,29 +1,5 @@
 import { Router } from '@boiling/core'
-import Schema from 'schemastery'
 
 export const router = new Router({
   prefix: '/channels'
-}).post(Schema.string(), '/', async ctx => {
-    return 'you are creating channel.'
-  })
-  .del(Schema.string(), '/:id', async ctx => {
-    return 'you are deleting channel'
-  })
-  .patch(Schema.string(), '/:id', async ctx => {
-    return 'you are patching channel'
-  })
-  .get(Schema.string(), '/', async ctx => {
-    return 'you are searching channels'
-  })
-  .get(Schema.string(), '/:id', async ctx => {
-    return 'you are getting channel'
-  })
-  .get(Schema.string(), '/:id/members', async ctx => {
-    return 'you are getting channel members.'
-  })
-  .post(Schema.string(), '/:id/members', async ctx => {
-    return 'you join to channel.'
-  })
-  .post(Schema.string(), '/:id/messages', async ctx => {
-    return 'you are posting message.'
-  })
+})
