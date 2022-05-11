14554fcb9479b254b518f089a05daeb56f2c58b2
yijie4188 <yijie4188@gmail.com>
2022-05-11 23:28:24 +0800

    feat(backend): save upload files and when it exsits skip copy file

--- packages/backend/src/routes/common.ts
+++ packages/backend/src/routes/common.ts
@@ -1,5 +1,8 @@
 import { Router } from '@boiling/core'
 import { File } from 'formidable'
+import { cp, pathExists } from 'fs-extra'
+import * as path from 'path'
+import { staticPath } from '../index'
 
 export const router = new Router({
   prefix: '/common'
@@ -8,5 +11,16 @@ export const router = new Router({
     const files = Object.entries(ctx.request.files ?? {}).reduce((acc, [_, arg]) => {
       return acc.concat(Array.isArray(arg) ? arg : [ arg ])
     }, [] as File[])
-    console.log('upload files:', files)
+    return await Promise.all(files.map(async file => {
+      const ext = require('mime-types').extension(file.mimetype)
+      const targetPath = path.resolve(staticPath, 'uploads', `${ file.hash }.${ ext }`)
+      if (await pathExists(targetPath))
+        return targetPath
+
+      return new Promise<string>((resolve, reject) => {
+        if (!file.hash)
+          throw new Error('file.hash is not defined')
+        cp(file.filepath, targetPath, err => err ? reject(err) : resolve(targetPath))
+      })
+    }))
   })
