8480848461662f196373eaf0eae1b0e1695d7055
yijie4188 <yijie4188@gmail.com>
2022-05-11 22:55:27 +0800

    refactor(backend): remove bodyparser and remove formidable

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,11 +1,10 @@
 import Koa from 'koa'
 import session from 'koa-session'
 import websockify from 'koa-websocket'
-import bodyParser from 'koa-bodyparser'
+import koaBody from 'koa-body'
 import staticMiddleware from 'koa-static'
 import logger from 'koa-logger'
-import path, { resolve } from 'path'
-import koaBody from 'koa-body'
+import { resolve } from 'path'
 
 import './global'
 import DAOMain from './dao'
@@ -15,6 +14,7 @@ import { router as WSRouter } from './routes/ws'
 import { router as UsersRouter } from './routes/users'
 import { router as ChannelsRouter } from './routes/channels'
 import { router as ChatRoomsRouter } from './routes/chat-rooms'
+import { router as CommonRouter } from './routes/common'
 import { Middlewares } from './middlewares'
 
 configDotenv()
@@ -22,27 +22,22 @@ configDotenv()
 const app = websockify(new Koa())
 app.keys = ['hker92hjkugfkerbl.e[gewkg68']
 
+export const staticPath = resolve(__dirname, '../static')
+
 app.ws.use(WSRouter)
 app
-  .use(staticMiddleware(resolve(__dirname, '../static')))
-  .use(koaBody({
-    // 支持文件格式
-    multipart: true,
-    formidable: {
-      // 上传目录
-      uploadDir: path.join(__dirname, 'static/uploads'),
-      // 保留文件扩展名
-      keepExtensions: true
-    }
-  }))
+  .use(staticMiddleware(staticPath))
   .use(logger())
-  .use(bodyParser())
+  .use(koaBody({
+    multipart: true
+  }))
   .use(session(app))
   .use(Middlewares.handleErrors)
   .use(Middlewares.returnBody)
   .use(UsersRouter.middleware())
   .use(ChannelsRouter.middleware())
   .use(ChatRoomsRouter.middleware())
+  .use(CommonRouter.middleware())
 
 const {
   BACKEND_PORT: PORT = '8080',
