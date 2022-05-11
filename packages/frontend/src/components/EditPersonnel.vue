696f758d5fc17a905f55a44823c84123b8b94b69
Only566 <201803020134@stu.hnie.edu.cn>
2022-05-11 20:25:00 +0800

    upd(frontend): edite personnel display

--- packages/frontend/src/components/EditPersonnel.vue
+++ packages/frontend/src/components/EditPersonnel.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="edit-personnel">
     <el-page-header :icon="ArrowLeft" title="返回" content="编辑个人信息" @back="$router.push('/home')" />
-    <user-info
-      :info="store.state.user"
-      :type="'popup'"/>
-    <el-form ref="editPersonnelForm" :model="form" label-width="80px">
+    <el-avatar
+      class="avatar"
+      :size="120"
+      :src="`/api/${store.state.user.avatar}`"
+      @click="$refs.avatar.show()"
+    />
+    <el-divider content-position="left">基本信息</el-divider>
+    <el-form ref="editPersonnelForm" :model="form" label-width="75px">
       <el-form-item label="用户名">
         <el-input
           v-model="form.username"/>
@@ -26,11 +30,36 @@
         <el-input v-model="form.desc" type="textarea" rows="6"/>
       </el-form-item>
     </el-form>
+    <el-divider content-position="left">密码</el-divider>
+    <el-button type="primary" @click="pwd.show = true">更改密码</el-button>
     <div class="bottom">
       <el-button type="primary" @click="onSubmit">确认</el-button>
       <el-button @click="$router.push('/home')">取消</el-button>
     </div>
   </div>
+  <el-dialog v-model="pwd.show" title="更改密码">
+    <el-form :model="pwd" label-width="100px" label-position="top">
+      <el-form-item label="当前密码">
+        <el-input v-model="pwd.oldPwd" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="新密码">
+        <el-input v-model="pwd.newPwd" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="确认新密码">
+        <el-input v-model="pwd.confirmPwd" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="pwd.show = false">取消</el-button>
+        <el-button type="primary" @click="pwd.show = false">确认</el-button>
+      </span>
+    </template>
+  </el-dialog>
+  <avatar
+    ref="avatar"
+    @selAvatar="(selAvatar) => changeAvatar(selAvatar)"
+  />
 </template>
 
 <script lang="ts" setup>
@@ -43,27 +72,54 @@ import {
   ElDatePicker,
   ElInput,
   ElButton,
-  ElMessage
+  ElMessage,
+  ElAvatar,
+  ElDivider,
+  ElDialog
 } from 'element-plus'
 import { ArrowLeft } from '@element-plus/icons-vue'
 import { computed, onMounted, onUnmounted, ref } from 'vue'
 import store from '../store'
 import { Users } from '@boiling/core'
-import UserInfo from './User.vue'
+import Avatar from './Avatar.vue'
+import { ElMessageBox } from 'element-plus'
+
+type PWD = {
+  show: boolean
+  oldPwd: string,
+  newPwd: string,
+  confirmPwd: string
+}
 
 const
   user = computed(() => store.state.user),
   form = ref<Users.UpdateOut>({
-  username: '',
-  sex: 'female',
-  birthday: '',
-  desc: ''
-})
+    username: '',
+    sex: 'female',
+    birthday: '',
+    desc: ''
+  }),
+  pwd = ref<PWD>({
+    show: false,
+    oldPwd: '',
+    newPwd: '',
+    confirmPwd: ''
+  })
 
-const onSubmit = async () => {
-  await store.dispatch('update', form.value)
-  ElMessage.success('更新成功！')
-}
+const
+  onSubmit = async () => {
+    await store.dispatch('update', form.value)
+    ElMessage.success('更新成功！')
+  },
+  changeAvatar = async (avatar: string) => {
+    await ElMessageBox.confirm(
+      '是否确认更换头像？', '确认', {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消'
+      }
+    )
+    await store.dispatch('updAvatar', avatar)
+  }
 
 onMounted(() => {
   store.commit('setLeftSelectorHidden', true)
@@ -88,15 +144,22 @@ onUnmounted(() => {
   .el-page-header {
     margin-bottom: 20px;
   }
+  > .avatar {
+    margin: 0 auto;
+    display: block;
+    cursor: pointer;
+  }
+  > .el-form {
+    > .el-form-item {
+      ::v-deep(.el-input) {
+        width: 212px;
+      }
+    }
+  }
   > div.bottom {
     position: absolute;
     right: 40px;
     bottom: 60px;
-  }
-  > .user {
-    margin-left: 90px;
-    margin-bottom: 20px;
-    width: calc(100% - 110px);
   }
 }
 </style>
