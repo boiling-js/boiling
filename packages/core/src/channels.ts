680fc7dc7c8f5e7056df176b37b2e1b1be9f2f3c
yijie4188 <yijie4188@gmail.com>
2022-05-15 17:19:45 +0800

    feat(core): channel detail content

--- packages/core/src/channels.ts
+++ packages/core/src/channels.ts
@@ -1,2 +1,37 @@
+import Schema from 'schemastery'
+
 export namespace Channels {
+  export const MemberMeta = Schema.interface({
+    /** id */
+    id: Schema.string(),
+    /** name */
+    name: Schema.string()
+  })
+  export type MemberMeta = Schema.InferS<typeof MemberMeta>
+  export const ChatRoomMeta = Schema.interface({
+    /** id */
+    id: Schema.string(),
+    /**
+     * 从属的子标题
+     */
+    subTitle: Schema.string().optional()
+  })
+  export type ChatRoomMeta = Schema.InferS<typeof ChatRoomMeta>
+  export const Model = Schema.interface({
+    /** id */
+    id: Schema.string(),
+    /** 名称 */
+    name: Schema.string(),
+    /** 头像 */
+    avatar: Schema.string().optional(),
+    /** 成员列表 */
+    members: Schema.array(MemberMeta),
+    /** 聊天室列表 */
+    chatrooms: Schema.array(ChatRoomMeta).optional(),
+    /** 介绍信息 */
+    description: Schema.string(),
+    /** 创建时间 */
+    createdAt: Schema.from(Date).optional()
+  })
+  export type Model = Schema.InferS<typeof Model>
 }
