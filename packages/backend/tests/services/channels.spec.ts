fdc957bfe9036d894cf6bdc88eeff8b0cfa71169
Only566 <201803020134@stu.hnie.edu.cn>
2022-05-16 17:14:45 +0800

    feat(backend): channel operate and test

---
+++ packages/backend/tests/services/channels.spec.ts
@@ -0,0 +1,52 @@
+import { expect, use } from 'chai'
+import cap from 'chai-as-promised'
+import { ChannelsService } from '../../src/services/channels'
+import { Users } from '@boiling/core'
+import { UsersService } from '../../src/services/users'
+
+use(cap)
+
+after(() => {
+  process.exit(0)
+})
+
+describe('Channels Service', () => {
+  let u0: Users.Base
+  after(async () => {
+    await UsersService.Model.deleteMany({})
+  })
+
+  before(async () => {
+    u0 = await UsersService.add({ username: '001', passwordHash: '001', avatar: '001' })
+  })
+
+  afterEach(async () => {
+    await ChannelsService.Model.deleteMany({})
+  })
+  it('should create a channel', async () => {
+    const channel = await ChannelsService.create(u0.id, { name: 'test', avatar: 'test', description: 'test' })
+    expect(channel.name).to.equal('test')
+    expect(channel.avatar).to.equal('test')
+    expect(channel.description).to.equal('test')
+  })
+  it('should exits channel', async () => {
+    const channel = await ChannelsService.create(u0.id, { name: 'test', avatar: 'test', description: 'test' })
+    expect(await ChannelsService.exists(channel.id)).to.equal(true)
+    expect(await ChannelsService.exists('62820e4c8d68d496c4e8ce97')).to.equal(false)
+  })
+  it('should exits channel and throw error', async () => {
+    expect(await ChannelsService.exists('62820e4c8d68d496c4e8ce97')).to.equal(false)
+    await expect(ChannelsService.existsOrThrow('62820e4c8d68d496c4e8ce97'),
+      'throw a `NOT_FOUND` error'
+    ).to.be.eventually.rejectedWith('[404] id 为 \'62820e4c8d68d496c4e8ce97\' 的频道不存在')
+  })
+  it('should search channel', async () => {
+    await Promise.all([
+      ChannelsService.create(u0.id, { name: 'name', avatar: 'avatar', description: 'des' }),
+      ChannelsService.create(u0.id, { name: 'name1', avatar: 'avatar1', description: 'description1' }),
+      ChannelsService.create(u0.id, { name: 'name2', avatar: 'avatar2', description: 'description2' })
+    ])
+    expect(await ChannelsService.search('test')).to.be.have.lengthOf(0)
+    expect(await ChannelsService.search('name')).to.be.have.lengthOf(3)
+  })
+})
