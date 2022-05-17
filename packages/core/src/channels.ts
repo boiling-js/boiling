f7334a20dff6bb4dafe1a3f77056fa801575b68e
Only566 <201803020134@stu.hnie.edu.cn>
2022-05-17 22:40:48 +0800

    feat(core): change chatRooms to subChannel

--- packages/core/src/channels.ts
+++ packages/core/src/channels.ts
@@ -13,12 +13,17 @@ export namespace Channels {
   export const ChatRoomMeta = Schema.interface({
     /** id */
     id: Schema.string(),
-    /**
-     * 从属的子标题
-     */
-    subTitle: Schema.string().optional()
+    /** desc */
+    desc: Schema.string().optional()
   })
   export type ChatRoomMeta = Schema.InferS<typeof ChatRoomMeta>
+  export const SubChannelMeta = Schema.interface({
+    /** 标题 */
+    subTitle: Schema.string(),
+    /** 聊天室列表 */
+    chatRooms: Schema.array(ChatRoomMeta).default([]).optional()
+  })
+  export type SubChannelMeta = Schema.InferS<typeof SubChannelMeta>
   export const Model = Schema.interface({
     /** id */
     id: Schema.string(),
@@ -28,8 +33,8 @@ export namespace Channels {
     avatar: Schema.string(),
     /** 成员列表 */
     members: Schema.array(MemberMeta),
-    /** 聊天室列表 */
-    chatrooms: Schema.array(ChatRoomMeta).optional(),
+    /** 子频道 */
+    subChannel: Schema.array(SubChannelMeta),
     /** 介绍信息 */
     description: Schema.string().optional(),
     /** 创建时间 */
