c99095c21c5a6188d9d99c4e9f2a04381fc38617
Only566 <201803020134@stu.hnie.edu.cn>
2022-05-30 13:53:51 +0800

    fix(backend): get groups filter channel chat room

--- packages/backend/src/services/chat-rooms.ts
+++ packages/backend/src/services/chat-rooms.ts
@@ -114,7 +114,8 @@ export namespace ChatRoomsService {
    */
   export async function getGroups(uid: number) {
     return Model.find({
-      members: { $in: [ uid ], $not: { $size: 2 } }
+      members: { $in: [ uid ], $not: { $size: 2 } },
+      channelId: { $exists: false }
     })
   }
   /**
